64a39043c324569e4f23db0354ce0b8976d03e05
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2013-01-20 19:33:27 +0000

    Activate Django Admin Panel + Tutorial Model Changes

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
 from mysite.views import *
 
 urlpatterns = patterns('',
     (r'^$', homepage),
-    (r'^time/?$', current_datetime),
-    (r'^time/plus/(\d{1,2})/?$', hours_ahead),
+    (r'^admin/', include(admin.site.urls)),
+    (r'^time/$', current_datetime),
+    (r'^time/plus/(\d{1,2})/$', hours_ahead),
 )
