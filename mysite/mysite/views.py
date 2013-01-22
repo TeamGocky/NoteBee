52c775594475bff31639656b831506a9d2763e3d
Craig McLaughlin <mr_mcl@live.co.uk>
2013-01-22 18:25:22 +0000

    Moved home page out of base template.

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,9 +1,10 @@
 from django.http import Http404
+from django.template import RequestContext
 from django.shortcuts import render_to_response
 import datetime
 
 def homepage(request):
-	return render_to_response('base.html');
+	return render_to_response('home.html', context_instance=RequestContext(request));
 
 def current_datetime(request):
 	now = datetime.datetime.now()
