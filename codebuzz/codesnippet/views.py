29a2eb194749fbca1b8c97dbd08cfa38254779f1
CraigMcL <mr_mcl@live.co.uk>
2013-02-15 22:20:19 +0000

    Added user authentication check and formatted HTML.

--- codebuzz/codesnippet/views.py
+++ codebuzz/codesnippet/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def index(request):
+    context = RequestContext(request)
     modes = ["clike/clike"]
-    return render_to_response('index.html', {"modes" : modes}) 
+    return render_to_response('index.html', {"modes" : modes},
+                              context) 
