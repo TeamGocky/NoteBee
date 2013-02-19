0dacfc8923e483fc7ae63f904946b6c1eb57f5c1
Craig McLaughlin <mr_mcl@live.co.uk>
2013-02-19 19:52:20 +0000

    Added creation/updating of SnippetRating entry for logged in user.

--- codebuzz/codesnippet/views.py
+++ codebuzz/codesnippet/views.py
@@ -1,5 +1,6 @@
 from random import randint, seed
 
+from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
@@ -27,8 +28,8 @@ def index(request):
         form = SnippetForm()
     return render_to_response('index.html', {"form" : form}, context)
 
-def get_rating(request, sid):
-    """Retrieve the rating of the snippet with id = sid by user.
+def get_rating(request, snippet):
+    """Retrieve the rating of the snippet by request user.
     
     Returns None if no such rating exists otherwise returns the rating.
     """
@@ -37,44 +38,73 @@ def get_rating(request, sid):
     # rating for the snippet, if there is one.
     if request.user.is_authenticated():
         try:
-            initial = SnippetRating.objects.get(snippet=sid,
-                                                user=request.user.id)
+            
+            initial = SnippetRating.objects.get(snippet=snippet,
+                                                user=request.user)
         except ObjectDoesNotExist:
-            pass
-    return initial
+            return None
+    return initial.rating
 
-def view_snippet(request, sid):
+def view_snippet(request, sid, errors=[]):
     """View the snippet with id = sid argument."""
     context = RequestContext(request)
     try:
         snippet = Snippet.objects.get(id=sid)
         ratings = SnippetRating.objects.filter(snippet=sid)
         # Set selected rating to the submitted rating for this user
-        initial = get_rating(request, sid)
+        initial = get_rating(request, snippet)
         if initial is None:
             initial = 1
-        rform = SnippetRatingForm(initial={"rating" : initial})
+        rform = SnippetRatingForm(initial={"rating" : int(initial)})
         total_rating = 0.0
         if len(ratings) > 0:
-            total_rating = sum(ratings) / len(ratings)
+            for r in ratings:
+                total_rating += r.rating
+            total_rating = total_rating / len(ratings)
         return render_to_response("codesnippet/view_snippet.html",
                               {"snippet" : snippet,
                                "rating" : total_rating,
-                               "rating_form" : rform},
+                               "rating_form" : rform,
+                               "errors" : errors},
                               context)
     except ObjectDoesNotExist:
-        errors = ["Snippet does not exist"]
+        errors += ["Snippet does not exist"]
         return render_to_response("codesnippet/view_snippet.html",
                                   {"errors" : errors},
                                   context)
 
+@login_required
 def submit_rating(request, sid):
     """Submits a user rating of a snippet."""
     context = RequestContext(request)
-    msg = None
+    errors = []
     if request.method == "POST":
-        msg = "Submitted rating", request.POST["rating"]
-    return view_snippet(request, sid)
+        try:
+            urating = int(request.POST["rating"])
+        except ValueError:
+            errors += ["Not a number"]
+        else:
+            values = SnippetRating.RATINGS
+            if (urating >= values[0][0] and
+                    urating <= values[len(values)-1][0]):
+                u = request.user
+                try:
+                    s = Snippet.objects.get(id=sid)
+                except ObjectDoesNotExist:
+                    errors += ["Snippet does not exist"]
+                else:
+                    try:
+                        srating = SnippetRating.objects.get(user=u,
+                                                            snippet=s)
+                    except ObjectDoesNotExist:
+                        srating = SnippetRating(user=u, snippet=s,
+                                                rating=urating)
+                    else:
+                        srating.rating = urating
+                    srating.save()
+            else:
+                errors += ["Out of range rating: {}".format(urating)]
+    return view_snippet(request, sid, errors)
 
 def view_random_snippet(request):
     """Select a random snippet for the database to view."""
