128f8affb8e330b5a54fd9032c966491e711d926
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2013-02-25 21:13:51 +0000

    Post when new user registers

--- codebuzz/registration/views.py
+++ codebuzz/registration/views.py
@@ -4,6 +4,12 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from codesnippet.views import getLatestSnippets
+try:
+	import twitter
+	twitterSuccess = True
+except:
+    print 'Twitter could not be imported'
+    twitterSuccess = False
 
 def register(request):
     # Context necessary for csrf token in form.
@@ -12,6 +18,12 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            if twitterSuccess:
+                try:
+                    api = twitter.Api(consumer_key='0aIAx6JBFjCRsVo2I6m5VQ', consumer_secret='ezaCJZNLJtTRaIogVvv08u3thnwSTtDMHmRMMs7lyk', access_token_key='1219662349-DJfNG23p2NLME6VAQv02gGNJXLiVQ1r99upKB0k', access_token_secret='P806WM9qbwg81Q1lGsVvRZ1Xl3PACurSmL1BWqCHgg')
+                    api.PostUpdate('Please welcome our new user "' + user.name + '" to our website!')
+                except:
+                    print 'Twitter posting failed.'
             msg = "Successfully registered, please login."
             return render_to_response("register.html", {"msg" : msg,
                 "latestSnippets" : getLatestSnippets()}, context)
