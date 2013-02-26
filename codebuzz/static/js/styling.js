5bcc4ed2de2b23ec00c752e80869d3fc4cd1d6a8
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2013-02-26 15:37:30 +0000

    Allow weeeeeealy small widths

---
+++ codebuzz/static/js/styling.js
@@ -0,0 +1,29 @@
+var $, window, document, visible;
+
+window.onresize = function () {
+    'use strict';
+    var windowWidth, articleWidth = $("article").width();
+    if (window.innerWidth) {
+        windowWidth = window.innerWidth;
+    } else if (document.compatMode === 'CSS1Compat' && document.documentElement && document.documentElement.offsetWidth) {
+        windowWidth = document.documentElement.offsetWidth;
+    } else if (document.body && document.body.offsetWidth) {
+        windowWidth = document.body.offsetWidth;
+    } else {
+        windowWidth = 800;
+    }
+    if (windowWidth < 800) {
+        $(".nav").css("display", "none");
+		$("#editor").css("width", "90%");
+		$(".container").css("width", "99%");
+    } else {
+        $(".nav").css("display", "block");
+		$("#editor").css("width", "100%");
+		$(".container").css("width", "95%");
+    }
+};
+
+window.onload = function () {
+    'use strict';
+    window.onresize();
+};
