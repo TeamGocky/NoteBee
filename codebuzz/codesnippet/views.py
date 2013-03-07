21562b67fa74bde97643cce723f2f0fe1213c447
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2013-03-07 19:28:49 +0000

    Error persistence bug fix
    Quick and dirty.

--- codebuzz/codesnippet/views.py
+++ codebuzz/codesnippet/views.py
@@ -84,6 +84,7 @@ def get_total_rating(snippet):
 def view_snippet(request, sid, errors=[]):
     """View the snippet with id = sid argument."""
     context = RequestContext(request)
+    errors = []
     try:
         snippet = Snippet.objects.get(id=sid)
         snippet.hits += 1
